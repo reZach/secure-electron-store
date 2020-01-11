f1135c6542d93e09fe5d9cbc4032006ce3cdd4c3
reZach <reZach@users.noreply.github.com>
2020-01-10 22:32:29 -0600

    actually have to run babel before we publish!

--- index.js
+++ index.js
@@ -12,11 +12,7 @@ function _defineProperties(target, props) { for (var i = 0; i < props.length; i+
 function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
 
 // https://dev.to/therealdanvega/creating-your-first-npm-package-2ehf
-var fs = require("fs");
-
 var path = require("path");
-
-var electron = require("electron");
 
 var REQUESTFILE = "SES-RequestFile";
 exports.REQUESTFILE = REQUESTFILE;
@@ -30,13 +26,13 @@ exports.WRITEFILEREPLY = WRITEFILEREPLY;
 var Bindings =
 /*#__PURE__*/
 function () {
-  function Bindings(ipcMain, options) {
+  function Bindings(ipcMain, fs, basePath, options) {
     _classCallCheck(this, Bindings);
 
     this.ipcMain = ipcMain;
-    var appPath = electron.app.getPath("userData");
+    this.fs = fs;
     var filename = typeof options.filename === "undefined" ? "config.json" : options.filename;
-    this.filepath = path.join(appPath, filename);
+    this.filepath = path.join(basePath, filename);
     this.bindEvents();
   }
 
@@ -46,11 +42,9 @@ function () {
       var _this = this;
 
       ipcMain.on(REQUESTFILE, function (IpcMainEvent, args) {
-        fs.access(_this.filepath, fs.constants.F_OK, function (error) {
+        _this.fs.access(_this.filepath, _this.fs.constants.F_OK, function (error) {
           if (!error) {
-            fs.readFile(_this.filepath, {
-              encoding: "utf-8"
-            }, function (error, data) {
+            _this.fs.readFile(_this.filepath, function (error, data) {
               IpcMainEvent.reply(REQUESTFILEREPLY, {
                 error: error,
                 data: data
@@ -65,9 +59,7 @@ function () {
         });
       });
       ipcMain.on(WRITEFILE, function (IpcMainEvent, args) {
-        fs.writeFile(_this.filepath, args.data, {
-          encoding: "utf-8"
-        }, function (error) {
+        _this.fs.writeFile(_this.filepath, args.data, function (error) {
           if (error) {
             IpcMainEvent.reply(WRITEFILEREPLY, {
               error: error
@@ -83,5 +75,4 @@ function () {
   return Bindings;
 }();
 
-var _default = Bindings;
-exports["default"] = _default;
+exports["default"] = Bindings;
