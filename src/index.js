1106704189b4686d036708c92120c0f9e9cec8eb
reZach <reZach@users.noreply.github.com>
2020-01-20 22:53:37 -0600

    adding msgpack and logging

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
+const msgpack = require("msgpack");
 const path = require("path");
 
 const defaultOptions = {
-    path: ""
+    debug: true,
+    msgpack: true,
+    path: "",
+    filename: "data.json"
 };
 
 // Electron-specific; must match mainIpc
@@ -12,27 +16,30 @@ export const writeConfigResponse = "WriteConfig-Response";
 
 export default class Store {
     constructor(options){
-        if (typeof options === "undefined"){
-            this.options = {...defaultOptions};
-        } else if (typeof options !== "object"){
-            throw "options must be of type 'object'!";
-        } else {
-            this.options = options;
+        this.options = defaultOptions;
+
+        if (typeof options !== "undefined"){
+            this.options = Object.assign(this.options, options);
         }
-        console.log(this.options);
-        this.options.path = path.join(this.options.path, "data.json");
+        this.options.path = path.join(this.options.path, this.options.filename);
 
         this.validSendChannels = [readConfigRequest, writeConfigRequest];
         this.validReceiveChannels = [readConfigResponse, writeConfigResponse];
     }
 
     preloadBindings(ipcRenderer) {
+        const { debug } = this.options;
+
         return {
             send: (channel, key, value) => {                
                 if (this.validSendChannels.includes(channel)){
                     if (channel === readConfigRequest){
+                        debug ? console.log(`[secure-electron-store]: renderer received ${channel}.`) : null;
+
                         ipcRenderer.send(channel, {key});
                     } else if (channel === writeConfigRequest){
+                        debug ? console.log(`[secure-electron-store]: renderer received ${channel}.`) : null;
+
                         ipcRenderer.send(channel, {key, value});
                     }
                 }
@@ -41,27 +48,32 @@ export default class Store {
                 if (this.validReceiveChannels.includes(channel)){
                     
                     // Deliberately strip event as it includes "sender"
-                    ipcRenderer.on(channel, (event, args) => func(args));                
+                    ipcRenderer.on(channel, (event, args) => {
+                        debug ? console.log(`[secure-electron-store]: renderer received ${channel}.`) : null;
+                        func(args);
+                    });
                 }
             }
         };
     }
 
     mainBindings(ipcMain, browserWindow, fs) {
-        const { path } = this.options;
+        const { debug, path, msgpack } = this.options;
 
-        console.log("mainBindings");
         ipcMain.on(readConfigRequest, (IpcMainEvent, args) => {
-            console.log(readConfigRequest);
+            debug ? console.log(`[secure-electron-store]: main received ${readConfigRequest}.`) : null;
+            
             fs.readFile(path, (error, data) => {
-                console.log(`${readConfigRequest} read file`);
-                browserWindow.webContents.send(readConfigResponse, data);
+                let dataToRead = msgpack ? msgpack.unpack(args) : JSON.parse(args);                
+                browserWindow.webContents.send(readConfigResponse, dataToRead[args.key]);
             });
         });
     
         ipcMain.on(writeConfigRequest, (IpcMainEvent, args) => {
-            console.log(`${writeConfigRequest} - path: ${path}`);
-            fs.writeFile(path, JSON.stringify(args), (error) => {
+            debug ? console.log(`[secure-electron-store]: main received ${writeConfigRequest}.`) : null;
+
+            let dataToWrite = msgpack ? msgpack.pack(args) : JSON.stringify(args);
+            fs.writeFile(path, dataToWrite, (error) => {
                 console.log(`${writeConfigRequest} write file`);
                 browserWindow.webContents.send(writeConfigResponse, true);
             });
